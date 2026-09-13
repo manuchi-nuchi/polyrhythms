18779d6aa9528003fb2e5985997e88b0a07172d3
Manuel Herrera Ojea <mherreraojea@gmail.com>
2026-09-13 21:47:43 +0200

    moving head

--- scripts/dancer/Dancer.cs
+++ scripts/dancer/Dancer.cs
@@ -6,17 +6,57 @@ using Godot.Collections;
 public partial class Dancer : Node3D
 {
 	[Export] Array<Joint> joints;
+    [Export] float maxBodyOffset = 1;
+    [Export] Node3D body;
+    Curve ease = new Curve();
+    float duration = 0.2f;
+
+
+    public override void _Ready()
+    {
+        ease.AddPoint(Vector2.Zero);
+        ease.AddPoint(Vector2.One);
+    }
 
 
     public override void _UnhandledInput(InputEvent @event)
     {
         if (@event is InputEventKey eventKey)
         {
-            if (eventKey.Pressed && eventKey.Keycode == Key.N)
+            if (eventKey.Pressed && eventKey.Keycode == Key.N && !@event.IsEcho())
             {
-                foreach (Joint joint in joints)
-                    joint.NewPose(true);
+                NewPose();
             }
+        }
+    }
+
+    void NewPose()
+    {
+        foreach (Joint joint in joints)
+            joint.NewPose();
+
+        MoveBody();
+    }
+
+    async void MoveBody()
+    {
+        Vector3 from = body.Position;
+        Vector3 to = new Vector3(
+            (float)GD.RandRange(-maxBodyOffset, maxBodyOffset),
+            (float)GD.RandRange(-maxBodyOffset, maxBodyOffset),
+            0
+        );
+        float t = 0, v, startTime = Time.GetTicksMsec(), now;
+
+        while (t < duration)
+        {
+            now = Time.GetTicksMsec();
+            t = (now - startTime) / 1000;
+            v = ease.Sample(t);
+
+            body.Position = from.Lerp(to, v);
+
+            await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
         }
     }
 }
--- scripts/dancer/Joint.cs
+++ scripts/dancer/Joint.cs
@@ -3,14 +3,49 @@ using System;
 
 public partial class Joint : Node3D
 {
-	[Export] double min;
-	[Export] double max;
+	[Export] double minAngle;
+	[Export] double maxAngle;
+	[Export] float duration = 0.2f;
+	[Export] Curve ease;
+
+
+	public override void _Ready()
+	{
+		NewPose(true);
+	}
 
 
 	Vector3 newRotation = Vector3.Zero;
 	public void NewPose(bool insta = false)
 	{
-		newRotation.Z = (float)GD.RandRange(min, max);
-		Rotation = newRotation;
+		newRotation.Z = (float)GD.RandRange(minAngle, maxAngle);
+
+		if (insta)
+		{
+			Rotation = newRotation;
+		}
+		else
+		{
+			MoveTo(newRotation.Z);
+		}
+	}
+
+	async void MoveTo(float to)
+	{
+		float from = Rotation.Z;
+		float t = 0, v;
+		float startTime = Time.GetTicksMsec(), now;
+
+		while (t < duration)
+		{
+			now = Time.GetTicksMsec();
+			t = (now - startTime) / 1000f;
+			v = t / duration;
+			newRotation.Z = Mathf.Lerp(from, to, ease.Sample(v));
+			
+			Rotation = newRotation;
+
+			await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+		}
 	}
 }
