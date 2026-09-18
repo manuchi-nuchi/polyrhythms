42d4be7bbed5e55cee29c05a3cf3f2d9dedb2231
Manuel Herrera Ojea <mherreraojea@gmail.com>
2026-09-18 11:28:15 +0200

    fewer sudden angle jumps

--- scripts/TempoManager.cs
+++ scripts/TempoManager.cs
@@ -4,16 +4,20 @@ using System;
 public partial class TempoManager : Node3D
 {
 	[Export] int bpm = 50;
+    float t = 0;
+    float maxT = 10000;
     public static float Angle = 0;
     public static float SecondsPerLoop;
     public static float Speed;
     public static int BPM;
+    float offset;
 
 
     public override void _Ready()
 	{
         ComputeSpeed();
 	}
+
 
     void ComputeSpeed()
     {
@@ -27,6 +31,8 @@ public partial class TempoManager : Node3D
             Speed = Mathf.Pi * 2 / SecondsPerLoop;
         }
 
+        offset = Angle - GetCurrentAngle();
+
         BPM = bpm;
 
         GD.Print("new tempo: " + bpm);
@@ -37,7 +43,22 @@ public partial class TempoManager : Node3D
 
 	public override void _Process(double delta)
 	{
-        Angle = Time.GetTicksMsec() / 1000f * Speed;
+        t += (float)delta;
+
+        // prevent sudden angle jumps
+        // i *think* it happened by overflowing
+        if (t > maxT)
+        {
+            t -= maxT;
+            offset = Angle - GetCurrentAngle();
+        }
+
+        Angle = GetCurrentAngle() + offset;
+    }
+
+    float GetCurrentAngle()
+    {
+        return t * Speed;
     }
 
 
